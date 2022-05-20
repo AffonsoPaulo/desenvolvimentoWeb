cb93fc42f1a6a9fd79446ffda2749c1e7700b199
Your Name <you@example.com>
2022-05-20 17:12:36 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    $idade = 40;
+    echo "$idade </br>";
+    $titulosMundiaisDoVasco = 0;
+    echo "$titulosMundiaisDoVasco </br>";
+    $num = -123;
+    echo "$num </br>";
+    $num = 0123;
+    echo "$num </br>";
+    $num = 0x7B;
+    echo "$num </br>";
+    $precoDaCamisaDoFluminense = 1.99;
+    echo "$precoDaCamisaDoFluminense </br>";
+    $valor = -1.9;
+    echo "$valor </br>";
+    $maiorDeIdade = true;
+    echo "$maiorDeIdade </br>";
+    $x = false;
+    echo "$x </br>";
+    $dezEhMaiorQueNove = (10 > 9);
+    echo "$dezEhMaiorQueNove </br>";
+    $nome = 'Fulano';
+    echo "$nome </br>";
+    $nome = "Fulano";
+    echo "$nome </br>";
+?>
