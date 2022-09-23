f50b7ade10740c33eac80c8c26ba9f767fd832f6
Paulo Affonso <plpaulo25affonso@outlook.com.br>
2022-09-23 16:29:58 -0300

    AULA 10 e TRABALHO

--- trabalho/index.php
+++ trabalho/index.php
@@ -5,16 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
     <title>Formulário de Inscrição</title>
 </head>
 <body>
 <main>
     <form id="formData" name="formData" method="post" action="receive.php">
         <h1>Dados Pessoais:</h1>
-        <label for="formName">Nome: </label>
+        <div class="card">
+        <label class="inputTitles" for="formName">Nome: </label>
         <input id="formName" name="formName" type="text">
+        </div>
 
-        <label for="formCivil">Estado Civil: </label>
+        <div class="card">
+        <label class="inputTitles" for="formCivil">Estado Civil: </label>
         <select id="formCivil" name="formCivil">
             <option>Solteiro</option>
             <option>Casado</option>
@@ -23,28 +27,47 @@
             <option>Divorciado</option>
             <option>Separado</option>
         </select>
+        </div>
 
-        <label for="formBorn">Idade: </label>
-        <input id="formBorn" name="formBorn" type="number">
+        <div class="card">
+        <label class="inputTitles" for="formAge">Idade: </label>
+        <input id="formAge" name="formAge" type="number">
+        </div>
 
-        <label for="formEmail">E-mail: </label>
+        <div class="card">
+        <label class="inputTitles" for="formEmail">E-mail: </label>
         <input id="formEmail" name="formEmail" type="email">
+        </div>
 
-        <label for="formTel">Telefone: </label>
-        <input id="formTel" name="formTel" type="tel" maxlength="15" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}"
+        <div class="card">
+        <label class="inputTitles" for="formTel">Telefone: </label>
+        <input id="formTel" name="formTel" type="tel" maxlength="15"
                placeholder="(22) 99999-9999">
+        </div>
 
-        <label for="formAboutMe">Sobre mim: </label>
+        <div class="card">
+        <label class="inputTitles" for="formAboutMe">Sobre mim: </label>
         <input id="formAboutMe" name="formAboutMe" type="text">
+        </div>
 
-        <label for="formSchool">Formação Acadêmica: </label>
+        <div class="card">
+        <label class="inputTitles" for="formSchool">Formação Acadêmica: </label>
         <input id="formSchool" name="formSchool" type="text">
+        </div>
 
-        <label for="formGoals">Objetivos: </label>
+        <div class="card">
+        <label class="inputTitles" for="formGoals">Objetivos: </label>
         <input id="formGoals" name="formGoals" type="text">
+        </div>
 
-        <label for="formLang">Idiomas: </label>
+        <div class="card">
+        <label class="inputTitles" for="formLang">Idiomas: </label>
         <input id="formLang" name="formLang" type="text">
+        </div>
+
+        <div class="card">
+        <input id="formSubmit" name="formSubmit" type="submit" value="Submit">
+        </div>
     </form>
 </main>
 </body>
--- trabalho/receive.php
+++ trabalho/receive.php
@@ -1,1 +1,20 @@
 <?php
+if(!empty($_POST['formName']))
+    echo 'Nome: ', $_POST['formName'],'</br>';
+if(!empty($_POST['formCivil']))
+    echo 'Estado Civil: ', $_POST['formCivil'],'</br>';
+if(!empty($_POST['formAge']))
+    echo 'Idade: ', $_POST['formAge'],'</br>';
+if(!empty($_POST['formEmail']))
+    echo 'E-mail: ', $_POST['formEmail'],'</br>';
+if(!empty($_POST['formTel']))
+    echo 'Telefone: ', $_POST['formTel'],'</br>';
+if(!empty($_POST['formAboutMe']))
+    echo 'Sobre mim: ', $_POST['formAboutMe'],'</br>';
+if(!empty($_POST['formSchool']))
+    echo 'Formação Acadêmica: ', $_POST['formSchool'],'</br>';
+if(!empty($_POST['formGoals']))
+    echo 'Objetivos: ', $_POST['formGoals'],'</br>';
+if(!empty($_POST['formLang']))
+    echo 'Idiomas: ', $_POST['formLang'],'</br>';
+?>
